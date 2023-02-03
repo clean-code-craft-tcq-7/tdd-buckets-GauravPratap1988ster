4e41323e46c2c34e3705e890191b6af0f9e9d00b
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2023-02-03 14:53:01 +0530

    Delete output_in_csv_format.py

--- output_in_csv_format.py
+++
@@ -1,7 +0,0 @@
-def create_csv_format_string(continuous_range, number_of_readings):
-    csv_string = "Range, Readings"
-    for value in range(len(continuous_range)):
-        value_key = "{}-{}".format(continuous_range[value][0],continuous_range[value][1])
-        csv_string += "\n{}, {}".format(value_key, number_of_readings[value])
-
-    return csv_string
